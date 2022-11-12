f49e3209d4b61611e3e920d58e9572c4198281ee
miranda <lfms@cesar.org.br>
2022-11-12 20:22:07 -0300

    Change proj. to run on Esp8266

--- ProjetoM.ino
+++ ProjetoM.ino
@@ -1,81 +1,61 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiAP.h>
+#include <ESP8266WiFi.h>
+#include "FS.h"
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-#define LED_BUILTIN 10  
+const char *ssid = "ESP8266";
+const char *password = "987654321";
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
 
-// Set these to your desired credentials.
-const char *ssid = "ProjetoM_AP";
+AsyncWebServer  server(80);
 
-WiFiServer server(80);
+void setup()
+{
+ 	Serial.begin(115200);
+  configWiFiMode();
 
-
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("Configuring access point...");
-
-  // You can remove the password parameter if you want the AP to be open.
-  WiFi.softAP(ssid);
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  String newHostname = "ESP32Node";
-  WiFi.hostname(newHostname.c_str());
-  server.begin();
-
-  Serial.println("Server started");
+}
+void loop() {
+ 	
 }
 
-void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
+bool configWiFiMode(){
+  Serial.print("Setting soft-AP configuration ... ");
+ 	Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+ 	Serial.print("Setting soft-AP ... ");
+ 	Serial.println(WiFi.softAP(ssid) ? "Ready" : "Failed!");
+ 	Serial.print("Soft-AP IP address = ");
+ 	Serial.println(WiFi.softAPIP());
 
-  if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+  server.begin();
+  Serial.println("Servidor iniciado"); 
+ 
+  Serial.print("IP para se conectar ao NodeMCU: "); 
+  Serial.print("http://"); 
+  Serial.println(WiFi.softAPIP());
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return false;
   }
+
+  File f = SPIFFS.open("/wifimanager.html", "r");
+  if(!f){
+    Serial.println("File error");
+    return false;
+  }
+  f.close();
+
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+  
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/wifimanager.html", "text/html");
+  });
+  server.begin();
+  return true;
 }
